5393772d0cf3e05a0bc22fff3344eba962a6d976
WinterKwon <winterkwon7017@gmail.com>
2022-06-03 22:58:23 +0900

    product validator 주석 추가

--- src/middlewares/product-validator.js
+++ src/middlewares/product-validator.js
@@ -1,5 +1,9 @@
 import {body, validationResult} from 'express-validator';
 
+
+// 상품 등록 및 수정시 price 타입 체크 - 숫자형 아니면 에러 반환
+// 프론트와 정책 통일 
+
 const validate = (req,res,next) => {
     
     const errors = validationResult(req);
